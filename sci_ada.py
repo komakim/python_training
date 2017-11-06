2e6429f2b36302363cf7e07d3cab73a62bddd832
masako-k <masako-k@masako-k-no-MacBook-Air.local>
2017-11-06 23:55:31 +0900

    adaboost and sci_ada

--- sci_ada.py
+++ sci_ada.py
@@ -1,8 +1,5 @@
 print(__doc__)
 
-# Author: Noel Dawe <noel.dawe@gmail.com>
-#
-# License: BSD 3 clause
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,10 +11,10 @@ from sklearn.datasets import make_gaussian_quantiles
 
 # Construct dataset
 X1, y1 = make_gaussian_quantiles(cov=2.,
-                                 n_samples=200, n_features=2,
+                                 n_samples=100, n_features=2,
                                  n_classes=2, random_state=1)
 X2, y2 = make_gaussian_quantiles(mean=(3, 3), cov=1.5,
-                                 n_samples=300, n_features=2,
+                                 n_samples=200, n_features=2,
                                  n_classes=2, random_state=1)
 X = np.concatenate((X1, X2))
 y = np.concatenate((y1, - y2 + 1))
@@ -61,7 +58,7 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Decision Boundary')
 
-# Plot the two-class decision scores
+
 twoclass_output = bdt.decision_function(X)
 plot_range = (twoclass_output.min(), twoclass_output.max())
 plt.subplot(122)
